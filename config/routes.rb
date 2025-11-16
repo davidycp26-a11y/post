faf75b33e8aac5d485c26c73a308893b608a79d2
davidycp26-a11y <davidycp26@gmail.com>
2025-11-16 14:45:55 +0900

    Added basic homepage

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,8 @@
 Rails.application.routes.draw do
   resources :messages
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  get '/' => 'home#top'
+  get '/about' => 'home#about'
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
@@ -11,7 +12,8 @@ Rails.application.routes.draw do
   # get "service-worker" => "rails/pwa#service_worker", as: :pwa_service_worker
 
   # Defines the root path route ("/")
-  root "messages#index"
+  root "home#top"
+
   # root "posts#index"
   mount MissionControl::Jobs::Engine, at: "/jobs"
 end
