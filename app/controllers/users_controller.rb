0674d7af5b2e39038d272c469ae1d13fcb989c60
davidycp26-a11y <davidycp26@gmail.com>
2025-11-19 18:37:01 +0900

    Added user model-Sign up function without password

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,42 @@
+class UsersController < ApplicationController
+  def index
+    @users = User.all.order(created_at: :desc)
+  end
+
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(name: params[:user][:name], email: params[:user][:email])
+    if @user.save
+      flash[:notice] = "User was successfully created."
+      redirect_to user_path(@user)
+    else
+      logger.debug "Validation errors: #{@user.errors.full_messages.join(', ')}"
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:user][:name]
+    @user.email = params[:user][:email]
+    if @user.save
+      flash[:notice] = "User was successfully updated."
+      redirect_to user_path(@user)
+    else
+      logger.debug "Validation errors: #{@user.errors.full_messages.join(', ')}"
+      render :edit, status: :unprocessable_entity
+    end
+  end
+
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[8.1].define(version: 2025_11_16_081635) do
+ActiveRecord::Schema[8.1].define(version: 2025_11_19_084832) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_catalog.plpgsql"
 
@@ -167,6 +167,13 @@ ActiveRecord::Schema[8.1].define(version: 2025_11_16_081635) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.datetime "created_at", null: false
+    t.string "email"
+    t.string "name"
+    t.datetime "updated_at", null: false
   end
 
   add_foreign_key "solid_queue_blocked_executions", "solid_queue_jobs", column: "job_id", on_delete: :cascade
