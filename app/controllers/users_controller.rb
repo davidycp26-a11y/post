b046d92e655e8e443c60a97a538dcf4de1b9729e
davidycp26-a11y <davidycp26@gmail.com>
2025-11-30 17:13:38 +0900

    hide email from another user in profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,4 @@
 class UsersController < ApplicationController
-
   before_action :require_login, except: [:new, :create]
   before_action :require_logout, only: [:new, :create]
   before_action :ensure_correct_user, only: [:edit, :update]
